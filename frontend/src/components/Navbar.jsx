2d2193057d28b081c5f6f04d15919ddb614fd572
ansonRodrigues <ansonrodrigues18@gmail.com>
2025-12-16 02:19:10 +0530

    Hover on navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,9 +3,45 @@ import { assets } from "../assets/assets";
 
 const Navbar = () => {
   return (
-    <div className="h-16 flex items-center border-b">
-      <img src={assets.logo} alt="" className="w-40" />
-    </div>
+    <nav className="h-16 border-b px-6">
+      <div className="relative flex items-center h-full">
+        <img src={assets.logo} alt="Youtique" className="w-36" />
+
+        <ul
+          className="
+            hidden lg:flex absolute left-1/2 -translate-x-1/2
+            items-center gap-10
+            text-[15px] font-light tracking-wide text-gray-800
+
+            [&_a]:relative
+            [&_a]:cursor-pointer
+            [&_a]:transition-colors
+            [&_a:hover]:text-gray-500
+
+            [&_a::after]:content-['']
+            [&_a::after]:absolute
+            [&_a::after]:left-0
+            [&_a::after]:-bottom-1
+            [&_a::after]:w-0
+            [&_a::after]:h-px
+            [&_a::after]:bg-gray-400
+            [&_a::after]:transition-all
+            [&_a::after]:duration-300
+            [&_a:hover::after]:w-full
+          "
+        >
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>Collection</a>
+          </li>
+          <li>
+            <a>About</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
