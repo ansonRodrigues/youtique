11511f86807f285d3388a9d341c8ca0675d711ac
ansonRodrigues <ansonrodrigues18@gmail.com>
2025-12-15 21:53:01 +0530

    Installed tailwindcss and router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <div className="text-red-300 font-bold">Hello</div>
+    </>
+  );
 };
 
 export default App;
