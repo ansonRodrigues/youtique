b5250dfe8ab0c8e2b5f4f26a7c829d9cd760a01b
ansonRodrigues <ansonrodrigues18@gmail.com>
2026-01-03 18:15:22 +0530

    Fix assets file

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,27 +1,13 @@
 import logo from "./ui/logo.png";
 import aboutUs from "./ui/aboutUs.png";
 import notfoundPage from "./ui/404error.png";
-import loginImg from "./ui/login.png";
-import cart from "./ui/cart.png";
-import searchIcon from "./ui/search.png";
 import heroImg from "./ui/hero.jpg";
 import contactUs from "./ui/contact.png";
-import iG from "./ui/instagram.png";
-import twitter from "./ui/twitter.png";
-import phone from "./ui/phone.png";
-import email from "./ui/email.png";
 
 export const assets = {
   logo,
   aboutUs,
   notfoundPage,
-  loginImg,
-  cart,
-  searchIcon,
   heroImg,
   contactUs,
-  iG,
-  twitter,
-  phone,
-  email,
 };
